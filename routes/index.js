a99cfe1ab502ced3830dfe7aaf9d528b7a0bb6cc
Li Linhao <lhli@thoughtworks.com>
2018-08-30 21:31:04 +0800

    refactor project base structure

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.render('index', {title: 'Express'})
 });
 
-module.exports = router;
+module.exports = router
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+import express from 'express'
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+const router = express.Router()
+
+router.get('/', (req, res, next) => {
+  res.send('respond with a resource')
 });
 
-module.exports = router;
+module.exports = router
