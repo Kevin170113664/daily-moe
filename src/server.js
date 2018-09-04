7433f83c5047f0ae7922568a67d7e09d351171dd
Li Linhao <lhli@thoughtworks.com>
2018-09-04 13:38:35 +0800

    simplify dist directory

--- src/server.js
+++ src/server.js
@@ -2,7 +2,36 @@
 
 import http from 'http'
 import app from './app'
-import {normalizePort, onError} from './helper/start-up-helper'
+
+const normalizePort = (val) => {
+  const port = parseInt(val, 10)
+  if (isNaN(port)) return val
+  if (port >= 0) return port
+  return false
+}
+
+const onError = (error) => {
+  if (error.syscall !== 'listen') {
+    throw error
+  }
+
+  const bind = typeof port === 'string'
+    ? 'Pipe ' + port
+    : 'Port ' + port
+
+  switch (error.code) {
+    case 'EACCES':
+      console.error(bind + ' requires elevated privileges')
+      process.exit(1)
+      break
+    case 'EADDRINUSE':
+      console.error(bind + ' is already in use')
+      process.exit(1)
+      break
+    default:
+      throw error
+  }
+}
 
 const defaultPort = '3000';
 const port = normalizePort(process.env.PORT || defaultPort)
